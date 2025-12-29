8cc1399681165dab56cb418fec9e80122251be41
caesarKom <cezarykomorowski@gmail.com>
2025-12-29 18:33:25 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-otp-token','token']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-otp-token', 'x-media-token', 'token']
 }));
 
 // Rate limiting
